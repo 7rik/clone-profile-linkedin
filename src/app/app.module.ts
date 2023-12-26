55803c8fd687807fb1c33fe1b02a82ee5a146e7f
7rik <bezerra.erik2003@gmail.com>
2023-12-26 17:13:21 -0300

    feat: criado projeto e iniciado construção da toolbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProfilePageComponent } from './profile-page/profile-page/profile-page.component';
+import { ToolbarComponent } from './profile-toolbar/toolbar/toolbar.component';
+import { ProfileCardComponent } from './profile-card/profile-card/profile-card.component';
+import { AnnouncementComponent } from './announcement/announcement/announcement.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProfilePageComponent,
+    ToolbarComponent,
+    ProfileCardComponent,
+    AnnouncementComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
